28a6379006965a827f40fa168ff471694e46fad5
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-10-08 11:12:52 +0300

    Added products screen and its widgets

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id,this.title,this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(imageUrl,fit: BoxFit.cover,),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(title,textAlign: TextAlign.center,),
+          leading : const IconButton(icon: Icon(Icons.favorite,color: Colors.red,),onPressed: null,),
+          trailing: const IconButton(icon: Icon(Icons.shopping_cart,color: Colors.red,), onPressed: null,),
+        ),
+      ),
+    );
+  }
+}
