e5db80c8b66b4f89b5111c7905f3c707d922b192
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-10-11 10:50:42 +0300

    Completed product detail page

--- lib/screens/product_detail_page.dart
+++ lib/screens/product_detail_page.dart
@@ -12,6 +12,19 @@ class ProductDetailPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(loadedProduct.title),),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            width: double.infinity,
+            child: Image.network(loadedProduct.imageUrl,fit: BoxFit.cover,),
+          ),
+          const SizedBox(height: 10,),
+          Text('\$${loadedProduct.price}',style: const TextStyle(fontSize: 24,color: Colors.grey),),
+          const SizedBox(height: 10,),
+          Text(loadedProduct.description,textAlign: TextAlign.center,)
+        ],
+      ),
     );
   }
 }
