d50302851054b6c5f5b006ea8f1c5bf53494a8e5
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-10-13 10:20:21 +0300

    Checked favorite status with http patch method

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier{
   final String id;
@@ -20,6 +22,11 @@ class Product with ChangeNotifier{
   
   void toogleFavoriteStatus(){
     isFavorite = !isFavorite;
+    final url = Uri.https('flutter-app-3330c-default-rtdb.firebaseio.com','/products/$id.json');
+    http.patch(url,body: json.encode({
+      'isFavorite' : isFavorite,
+    }));
+
     notifyListeners();  
   }
 
